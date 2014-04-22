90a159d83d02589f8ab13eb88a941bede07c656a
Victor J Chamorro <victor@ipdea.com>
2014-04-22 18:59:56 +0200

    Add LICENSE texts

--- class/UTL/UTLHttp.php
+++ class/UTL/UTLHttp.php
@@ -1,8 +1,22 @@
 <?php
 /**
  * Clase de utilidades HTTP
- * @author VJChamorro
+ * @author Victor J Chamorro - victor@ipdea.com
+ * @copyright Ipdea Land, S.L.
  *
+ * LGPL v3 - GNU LESSER GENERAL PUBLIC LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU LESSER General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 class UTLHttp{
 
--- examples/prueba_soap.php
+++ examples/prueba_soap.php
@@ -1,4 +1,21 @@
-<?php 
+<?php
+/**
+ * @copyright Ipdea Land, S.L.
+ *
+ * LGPL v3 - GNU LESSER GENERAL PUBLIC LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU LESSER General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 ini_set("soap.wsdl_cache_enabled", 0);
 require_once '../class/UTL/UTLHttp.php';
 require_once '../class/ENV/MDL/ENVMDLEstadistica.php';
